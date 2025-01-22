1fb539033711076b1269a8af7df84b9a2df61121
Dmitry <dmitronus2015@gmail.com>
2025-01-22 17:07:43 +0200

    add task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", onLoginFormSubmit);
+
+function onLoginFormSubmit(event) {
+  event.preventDefault();
+
+  const useremail = event.target.elements.email;
+  const userpassword = event.target.elements.password;
+
+  if (!useremail.value || !userpassword.value) {
+    alert("Всі поля мають бути заповнені");
+  } else {
+    console.log({
+      useremail: useremail.value,
+      userpassword: userpassword.value,
+    });
+    event.target.reset();
+  }
+}
