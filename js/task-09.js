c3bab1a64364b2f97d0211735ed90375ffee2e4c
Dmitry <dmitronus2015@gmail.com>
2025-01-22 17:16:06 +0200

    add task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnElem = document.querySelector(".change-color");
+const colorText = document.querySelector(".color");
+
+btnElem.addEventListener("click", onBtnElemClick);
+
+function onBtnElemClick(elem) {
+  const randomColor = getRandomHexColor();
+  colorText.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
+}
