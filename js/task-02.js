6b02c6605f64cf80f2ce0664a8a6677d15468b5e
Dmitry <dmitronus2015@gmail.com>
2025-01-22 15:53:49 +0200

    add task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,28 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListElem = document.querySelector("#ingredients");
+
+function ingredientCreate(ingredient) {
+  const ingredientsElem = document.createElement("li");
+  ingredientsElem.classList.add("item");
+  ingredientsElem.textContent = ingredient;
+  return ingredientsElem;
+}
+
+function ingredientsCreate(ingredients) {
+  return ingredients.map(ingredientCreate);
+}
+
+function renderIngredint(ingredients) {
+  const ingredientsList = ingredientsCreate(ingredients);
+  ingredientsListElem.append(...ingredientsList);
+}
+
+renderIngredint(ingredients);
