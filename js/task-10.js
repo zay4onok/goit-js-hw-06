1e02045aaa059ebff01db6fc78de53714d781b66
Dmitry <dmitronus2015@gmail.com>
2025-01-22 17:44:54 +0200

    add task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const divBox = document.querySelector("#boxes");
+const addBtnElem = document.querySelector("[data-create]");
+const destroyBtnElem = document.querySelector("[data-destroy]");
+
+addBtnElem.addEventListener("click", onAddBtnElemClick);
+destroyBtnElem.addEventListener("click", onDestroyBtnElemClick);
+
+function createBoxes(amount) {
+  let divWidth = 30;
+  let divHeight = 30;
+  for (let i = 1; i <= amount; i++) {
+    const divElem = document.createElement("div");
+    divElem.style.width = divWidth + "px";
+    divElem.style.height = divHeight + "px";
+    divElem.style.backgroundColor = getRandomHexColor();
+    divBox.append(divElem);
+    divWidth += 10;
+    divHeight += 10;
+  }
+}
+
+function onAddBtnElemClick(event) {
+  const numberInput = +addBtnElem.previousElementSibling.value;
+  createBoxes(numberInput);
+}
+
+function onDestroyBtnElemClick(elem) {
+  addBtnElem.previousElementSibling.value = "";
+  divBox.innerHTML = "";
+}
